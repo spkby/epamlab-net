093cdcb2eafe51776d6cb1b1be93cdf713e094e9
VlaShip <vlaship@bk.ru>
2018-04-01 19:55:47 +0300

    classes1

---
+++ classes/Classes1/Classes1/BusinessTrip.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes1
+{
+	public class BusinessTrip
+	{
+		public const int Rate = 700;
+		public string Employee { get; set; }
+		public int TransportationExpenses { get; set; }
+		public int Days { get; set; }
+
+		public BusinessTrip()
+		{ }
+
+		public BusinessTrip(string employee, int transportationExpenses, int days)
+		{
+			TransportationExpenses = transportationExpenses;
+			Employee = employee;
+			Days = days;
+		}
+
+		public int GetTotal()
+		{
+			return TransportationExpenses + Rate * Days;
+		}
+
+		public void Show()
+		{
+			Console.WriteLine("rate = " + Rate);
+			Console.WriteLine("account = " + Employee);
+			Console.WriteLine("transport = " + TransportationExpenses);
+			Console.WriteLine("days = " + Days);
+			Console.WriteLine("total = " + GetTotal());
+		}
+
+		public override String ToString()
+		{
+			return Rate + ";" + Employee + ";" + TransportationExpenses + ";" + Days + ";" + GetTotal();
+		}
+	}
+}
---
+++ classes/Classes1/Classes1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			BusinessTrip[] businessTrips = {
+								new BusinessTrip("Alexandr Alexandrov", 2301, 3),
+								new BusinessTrip("Petr Petrov", 1168, 4),
+								null,
+								new BusinessTrip("Ivan Ivanov", 2301, 5),
+								new BusinessTrip()
+				};
+
+			foreach (BusinessTrip trip in businessTrips)
+			{
+				if (trip != null)
+				{
+					trip.Show();
+				}
+			}
+
+			if (businessTrips[businessTrips.Length - 1] != null)
+			{
+				businessTrips[businessTrips.Length - 1].Employee = "Nikolay Nokilaev";
+				businessTrips[businessTrips.Length - 1].TransportationExpenses = 1365;
+				businessTrips[businessTrips.Length - 1].Days = 2;
+			}
+
+			Console.WriteLine("Duration = " + (businessTrips[0].Days + businessTrips[1].Days));
+
+			foreach (BusinessTrip trip in businessTrips)
+			{
+				Console.WriteLine(trip);
+			}
+
+			Console.Read();
+		}
+	}
+}
