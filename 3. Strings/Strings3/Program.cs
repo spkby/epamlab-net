09d9e15044a795516432bfe2f10c509e5eb9341b
vlaship <vlaship@bk.ru>
2018-04-27 14:44:27 +0300

    Strings3

--- 3. Strings/Strings3/Program.cs
+++ 3. Strings/Strings3/Program.cs
@@ -1,12 +1,68 @@
 ﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Strings3
 {
-    internal class Program
+    internal static class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine();
+            // match quotation
+            const string quotation = "([^']\".*\")";
+            // match miltilines comment
+            const string multiLinesCommnent = @"(\/\*([\s\S]*?)\*\/)";
+            // match line comments
+            const string lineComment = @"(\/\/.*\n)";
+
+            const string pattern = quotation + "|" + multiLinesCommnent + "|" + lineComment;
+
+            const int groupMultiLinesCommnent = 2;
+            const int groupLineComment = 4;
+
+            var newLine = Environment.NewLine;
+            const string emptyStr = "";
+
+            const string pathIn = @"..\..\input.txt";
+            const string pathOut = @"..\..\output.txt";
+
+            try
+            {
+                using (var reader = new StreamReader(pathIn))
+                using (var writer = new StreamWriter(pathOut))
+                {
+                    var lines = new StringBuilder();
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        lines.AppendLine(line);
+                    }
+
+                    var regex = new Regex(pattern);
+                    var matches = regex.Matches(lines.ToString());
+
+                    foreach (Match match in matches)
+                    {
+                        var groups = match.Groups;
+                        if (groups[groupMultiLinesCommnent].Value.Length > 0)
+                        {
+                            lines.Replace(groups[groupMultiLinesCommnent].Value, emptyStr);
+                        }
+
+                        if (groups[groupLineComment].Value.Length > 0)
+                        {
+                            lines.Replace(groups[groupLineComment].Value, newLine);
+                        }
+                    }
+
+                    writer.Write(lines);
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found");
+            }
         }
     }
 }
