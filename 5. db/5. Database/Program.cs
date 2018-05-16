e7563d72f932f59ff878d7e7c309974094fd399f
vlaship <vlaship@bk.ru>
2018-05-16 23:47:37 +0300

    DB init

---
+++ 5. db/5. Database/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Database
+{
+    internal class Program
+    {
+
+        private const string connString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=""C:\Projects\DB\db1.mdb""";
+        
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
