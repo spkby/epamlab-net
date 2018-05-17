2c68f95a02e122a59189809e5c5ff9b04b0c419f
vlaship <vlaship@bk.ru>
2018-05-17 15:40:01 +0300

    database add mdb

---
+++ 5. db/5. Database/Ado.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.OleDb;
+
+namespace Collections2
+{
+    public class Ado
+    {
+        private const string connectionString =
+            @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=""D:\dev\projects\epamlab-net\5. db\5. Database\db1.mdb""";
+
+        private const string insertFrequenciesCmd = "insert into Frequencies (number,line_id) values (?,?)";
+        private const string updateFrequenciesCmd = "update Frequencies where line_id = ?";
+        private const string clearFrequenciesCmd = "delete from Frequencies";
+
+        private const string clearCoordinatesCmd = "delete from Coordinates";
+        private const string insertCoordinatesCmd = "insert into Coordinates (x1,x2,y1,y2) values (?,?,?,?)";
+
+
+        private const string selectCmd = "SELECT ... AS len, Count(*) AS num FROM Coordinates GROUP BY ... ORDER BY ... ";
+
+        public void add(OleDbCommand cmd, OleDbConnection conn)
+        {
+            
+        }
+    }
+}
--- 5. db/5. Database/Program.cs
+++ 5. db/5. Database/Program.cs
@@ -1,13 +1,54 @@
-﻿namespace Database
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Collections2
 {
     internal class Program
     {
+        private const string path = @"..\..\in.txt";
+        private const string fileNotFound = "File not found";
+        private const string pattern = @"[\s(;)]+";
 
-        private const string connString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=""C:\Projects\DB\db1.mdb""";
-        
+ 
+        private const int positionX1 = 1;
+        private const int positionX2 = 2;
+        private const int positionY1 = 3;
+        private const int positionY2 = 4;
+
         public static void Main(string[] args)
         {
-            
+          /*  var lines = new LenNumCollections();
+            try
+            {
+                using (var sr = new StreamReader(path))
+                {
+                    var regex = new Regex(pattern);
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        var strings = regex.Split(line);
+                        var x1 = double.Parse(strings[positionX1]);
+                        var x2 = double.Parse(strings[positionX2]);
+                        var y1 = double.Parse(strings[positionY1]);
+                        var y2 = double.Parse(strings[positionY2]);
+                        lines.Add(new LenNum(Utils.CalcLen(x1, x2, y1, y2)));
+                    }
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine(fileNotFound);
+            }
+
+            Console.WriteLine(lines.Print());*/
         }
+
+
+        
+        
     }
 }
