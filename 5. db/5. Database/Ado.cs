036645f1db43281eff6367269a533b7ebc45437b
vlaship <vlaship@bk.ru>
2018-05-17 23:24:49 +0300

    DB add Ado add Program add right mdb

--- 5. db/5. Database/Ado.cs
+++ 5. db/5. Database/Ado.cs
@@ -1,25 +1,142 @@
-﻿using System.Data.OleDb;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
 
-namespace Collections2
+namespace Database
 {
     public class Ado
     {
-        private const string connectionString =
-            @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=""D:\dev\projects\epamlab-net\5. db\5. Database\db1.mdb""";
+        private const string insert = "insert into Frequencies (len,num) values (@len,@num)";
+        private const string clear = "delete from Frequencies";
 
-        private const string insertFrequenciesCmd = "insert into Frequencies (number,line_id) values (?,?)";
-        private const string updateFrequenciesCmd = "update Frequencies where line_id = ?";
-        private const string clearFrequenciesCmd = "delete from Frequencies";
+        private const string selectLenNum =
+            "SELECT ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)) AS len, Count(*) AS num FROM Coordinates" +
+            " GROUP BY x1,x2,y1,y2 ORDER BY len";
 
-        private const string clearCoordinatesCmd = "delete from Coordinates";
-        private const string insertCoordinatesCmd = "insert into Coordinates (x1,x2,y1,y2) values (?,?,?,?)";
+        private const string selectWhereLenMoreThanNum =
+            "SELECT len,num FROM Frequencies WHERE len > num";
 
 
-        private const string selectCmd = "SELECT ... AS len, Count(*) AS num FROM Coordinates GROUP BY ... ORDER BY ... ";
+        private static OleDbCommand cmd;
 
-        public void add(OleDbCommand cmd, OleDbConnection conn)
+        public static OleDbConnection GetConnection(string path)
         {
-            
+            var connectionString =
+                @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + path + "db1.mdb";
+            return new OleDbConnection(connectionString);
+        }
+
+        public static IEnumerable<LenNum> GetList(OleDbConnection conn)
+        {
+            var list = new List<LenNum>();
+
+            try
+            {
+                cmd = new OleDbCommand(selectLenNum, conn);
+                conn.Open();
+                
+                // TODO: ! No value given for one or more required parameters.
+
+                var dataReader = cmd.ExecuteReader();
+
+                while (dataReader != null && dataReader.Read())
+                {
+                    var len = dataReader.GetInt32(0);
+                    var num = dataReader.GetInt32(1);
+                    list.Add(new LenNum(len, num));
+                }
+            }
+            catch (OleDbException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return list;
+        }
+
+        private static void Clear(OleDbConnection conn)
+        {
+            try
+            {
+                cmd = new OleDbCommand(clear, conn);
+                conn.Open();
+                var res = cmd.ExecuteNonQuery();
+                if (res == -1)
+                {
+                    throw new Exception("Error delete from Frequencies");
+                }
+            }
+            catch (OleDbException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public static void Insert(IEnumerable<LenNum> list, OleDbConnection conn)
+        {
+            try
+            {
+                Clear(conn);
+
+                conn.Open();
+
+                foreach (var lenNum in list)
+                {
+                    cmd = new OleDbCommand(insert, conn);
+                    cmd.Parameters.AddWithValue("@len", lenNum.Len);
+                    cmd.Parameters.AddWithValue("@num", lenNum.Num);
+                    var res = cmd.ExecuteNonQuery();
+                    if (res == -1)
+                    {
+                        Console.WriteLine("Error insert");
+                    }
+                }
+            }
+            catch (OleDbException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public static IEnumerable<LenNum> GetWhereLenMoreThanNum(OleDbConnection conn)
+        {
+            var list = new List<LenNum>();
+
+            try
+            {
+                cmd = new OleDbCommand(selectWhereLenMoreThanNum, conn);
+                conn.Open();
+                var dataReader = cmd.ExecuteReader();
+
+                while (dataReader != null && dataReader.Read())
+                {
+                    var len = dataReader.GetInt32(0);
+                    var num = dataReader.GetInt32(1);
+                    list.Add(new LenNum(len, num));
+                }
+            }
+            catch (OleDbException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return list;
         }
     }
 }
---
+++ 5. db/5. Database/LenNum.cs
@@ -0,0 +1,19 @@
+﻿namespace Database
+{
+    public class LenNum
+    {
+        public readonly int Len;
+        public readonly int Num;
+        
+        public LenNum(int len, int num)
+        {
+            Len = len;
+            Num = num;
+        }
+
+        public override string ToString()
+        {
+            return (Len+";"+Num);
+        }
+    }
+}
