60c7e45fc601a2b80b3547213ed4a2f24f4fdf3c
vlaship <vlaship@bk.ru>
2018-06-07 19:03:16 +0300

    add Models

--- Restaurant/Restaurant/Models/Dish.cs
+++ Restaurant/Restaurant/Models/Dish.cs
@@ -1,14 +1,44 @@
-﻿namespace Restaurant.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Restaurant.Models
 {
     public class Dish
     {
-        public string title { get; }
-        public string desc { get; }
-        public int price { get; }
+        [Key]
+        [Required]
+        public int DishId { get; set; }
+        
+        [DisplayName("Dishname")]
+        [RegularExpression(@"[\w| ]*", ErrorMessage = "Error Dishname")]
+        [Required(ErrorMessage = "Empty Dishname", AllowEmptyStrings = false)]
+        [MaxLength(100)]
+        public string Dishname { get; set; }
+        
+        [DisplayName("Description")]
+        [RegularExpression(@"[\w| ]*", ErrorMessage = "Error Description")]
+        [Required(ErrorMessage = "Empty Description", AllowEmptyStrings = false)]
+        [MaxLength(255)]    
+        public string desc { get; set;}
+        
+        [DisplayName("Price")]
+        [RegularExpression(@"[\d| ]*", ErrorMessage = "Error Price")]
+        [Required(ErrorMessage = "Empty Price", AllowEmptyStrings = false)]
+        [MaxLength(255)]    
+        public int price { get; set;}
+        
+        [Required]
+        [DisplayName("CourseId")]
+        public int CourseId { get; set; }
 
-        public Dish(string title, string desc, int price)
+        [DisplayName("Course")]
+        [NotMapped]
+        public virtual Course Course { get; set; }
+
+        public Dish(string Dishname, string desc, int price)
         {
-            this.title = title;
+            this.Dishname = Dishname;
             this.desc = desc;
             this.price = price;
         }
---
+++ Restaurant/Restaurant/Models/Role.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Restaurant.Models
+{
+    public class Role
+    {
+        [Key]
+        [Required]
+        public int RoleId { get; set; }
+        
+        [DisplayName("Rolename")]
+        [RegularExpression(@"[\w| ]*", ErrorMessage = "Error Rolename")]
+        [Required(ErrorMessage = "Empty Rolename", AllowEmptyStrings = false)]
+        [MaxLength(100)]
+        public string RoleName { get; set; }
+
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
