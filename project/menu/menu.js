676ee809bd9c5665d015fc6245af99f9c431e99f
vlaship <vlaship@bk.ru>
2018-06-07 10:57:43 +0300

    jquery and id

--- project/menu/menu.js
+++ project/menu/menu.js
@@ -1,6 +1,7 @@
-var accItem = document.getElementsByClassName('menuItem');
 
-var accHD = document.getElementsByClassName('menuItemHeading');
+var accItem = $('.menuItem');
+
+var accHD = $('.menuItemHeading');
 
 for (i = 0; i < accHD.length; i++) {
     accHD[i].addEventListener('click', toggleItem, false);
@@ -8,10 +9,23 @@ for (i = 0; i < accHD.length; i++) {
 
 function toggleItem() {
     var itemClass = this.parentNode.className;
+
     for (i = 0; i < accItem.length; i++) {
         accItem[i].className = 'menuItem close';
     }
+
     if (itemClass == 'menuItem close') {
         this.parentNode.className = 'menuItem open';
+
+        var id = 0;
+
+        for (i = 0; i < accItem.length; i++) {
+            if (accItem[i].className == 'menuItem open') {
+                id = i;
+            }
+        }
+
+        $('#'+id).text("<p>Test" + id + "</p>");
+
     }
 }
